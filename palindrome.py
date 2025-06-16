326aa195b02e9877c28dad15633c32e83e900206
JDN-Spark <jnorris2575@conestogac.on.ca>
2025-06-16 14:52:50 -0400

    Step 1 - Test invalid data assertion

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,14 @@
 """
 Validates strings as palindromes.
 """
+
+
+def is_palindrome(words):
+    """Determine if inout is a palindrome
+    Args:
+    words (string): any string
+    Returns:
+    boolean: True if palindrome, otherwise False
+    """
+    if not isinstance(words, str):
+        raise ValueError('Not a string')
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,13 @@
 """
 Tests the palindrome module
 """
+
+import pytest
+import palindrome
+
+
+def test_invalid_datatype():
+    """Assert non-string raises a ValueError"""
+    with pytest.raises(Exception) as excinfo:
+        palindrome.is_palindrome(3)
+    assert str(excinfo.value) == 'Not a string'
